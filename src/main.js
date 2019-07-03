46ec3a3cdc494ba5c02c74ea2616f4b19480455d
Emerson Jordan and Erik Irgens <erik@>
2019-07-03 15:02:32 -0700

    Branch commit.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
+setInterval(function(){
+  console.log("a");
+}, 1000);
 $(document).ready(function() {
   $("#userInputForm").submit(function(event){
     let long = parseFloat($("#userLong").val());
@@ -15,35 +17,28 @@ $(document).ready(function() {
     // placeholder clouds
     let cloud_score = false;
     // placeholder date
-    let date = "2014-02-01";
+      // debugger;
+      //only works so far with 2014
+    let date = $("#userDate").val().toString();
+    let date2 = "1995-05-02";
+    let dateStringGetter = `${date}`;
+    if (date === dateStringGetter){
+      console.log("true");
+    }
     let resolution = 0.8816 - parseFloat($("#resolutionRange").val());
-    $.ajax({
-      url: `https://api.nasa.gov/planetary/earth/imagery/?lon=${long}&lat=${lat}&date=2014-02-01&cloud_score=${cloud_score}&api_key=${process.env.API_KEY}&dim=${resolution}`,
-      type: 'GET',
-      // data: {
-      //   format: 'json'
-      // },
-      success: function(response) {
-        $("#imageRow").html("<img src='" + `${response.url}` +"'</img>");
-        console.log(response.url);
-        console.log(response);
-        console.log(response.toString());
-        console.log("Success");
-      },
-      error: function() {
-        $("#imageRow").html("<h1>There was an error processing your request. Please try a new location or date.</h1>");
-        console.log("Error!");
-        console.log(`${resolution}`);
-      }
+
+    $.get(`https://api.nasa.gov/planetary/earth/imagery/?lon=${long}&lat=${lat}&cloud_score=${cloud_score}&api_key=${process.env.API_KEY}&dim=${resolution}`).then(function(response){
+      $("#imageRow").html("<img src='" + `${response.url}` +"'</img>");
+      console.log(response.url);
+      console.log(response);
+      console.log(response.toString());
+      console.log("Success");
+    }).fail(function(error){
+      $("#imageRow").html("<h1>There was an error processing your request. Please try a new location or date.</h1>");
+      console.log("Error!");
+      console.log(`${resolution}`);
     });
-
-// Date nust be given in year-mm-dd
-
-
-
-
-
-
+    // Date nust be given in year-mm-dd
     event.preventDefault();
   });
 });
